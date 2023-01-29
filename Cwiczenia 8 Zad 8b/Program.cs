3a653f42b7d1992b7617c9570d67376732e8ff01
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-29 16:45:03 +0100

    Add files via upload

---
+++ Cwiczenia 8 Zad 8b/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Cwiczenia_8_Zad_8b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KOLORY. \nWpisz kilka kolorów od ulubionego do najmniej lubianego");
+            Console.WriteLine(" ");
+
+            Stack<string> kolory = new Stack<string>();
+            string kolor;
+            string na_bok;
+
+            do
+            {
+                Console.WriteLine("Wpisz kolor (lub wciśnij ENTER aby zakończyć");
+                kolor = Console.ReadLine();
+
+                if (kolor == "")
+                    break;
+                else
+                    kolory.Push(kolor);
+
+
+            }while(true);
+
+            na_bok = kolory.Pop();
+
+            while (kolory.Count != 1)
+            {
+                kolory.Pop();
+            }
+
+            kolory.Push(na_bok);
+
+            foreach (string barwa in kolory)
+                Console.Write(barwa + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
